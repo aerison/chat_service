bc6b60a6b08649d6cd32f48881d8ccff2667e398
aerison <sonsoo1224@gmail.com>
2018-07-20 01:46:37 +0000

    chatroom model relation 설정

--- app/models/admission.rb
+++ app/models/admission.rb
@@ -1,4 +1,4 @@
 class Admission < ActiveRecord::Base
   belongs_to :user
-  belongs_to :chat_room
+  belongs_to :chat_room ,counter_cache: true  #admission_count라는 곳에 캐싱되어있게됨.
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  #채팅이랑 1:n
+  has_many :chats
+  #챗룸이랑 m:n
+  has_many :admissions
+  has_many :chat_rooms, through: :admissions
 end
